71fa45555de712f49ea476f5a67ed9833028c0e5
Hayk Avdalyan <avdalyanhayk@gmail.com>
2022-03-23 14:12:42 +0400

    My first git commit

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,51 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("Barev\n");
+        System.out.println("Lesson 1");
+
+        byte x = 127;
+        System.out.println(x);
+
+        short y = -4542;
+        System.out.println(y);
+
+        int a = 454654;
+        System.out.println(a);
+
+        long b = 4465454654L;
+        System.out.println(b);
+
+        // kamayakan amboxj tiv javaum int e
+
+        // kamayakan racional tiv javaum double e
+        float aa = 455.4f;
+        double bb = 45.4;
+
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+
+        char t = '$';
+        System.out.println(t);
+        char tt = '\u003F';
+        System.out.println(tt);
+
+
+        String s = "barev Lesson 1";
+        System.out.println(s);
+
+        String firstName = "Hayk";
+        String lastName = "Avdalyan";
+
+        System.out.println(firstName + " " + lastName);
+
+        int k = 95;
+        System.out.println("k = " + k);
+
+
+    }
+
+}
